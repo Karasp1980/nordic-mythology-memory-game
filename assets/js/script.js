0cddbd589341cf2a13572a5a33bf0ea817dd5e87
Karasp1980 <karasp80@gmail.com>
2022-11-19 15:17:49 +0000

    Add DOMContentLoaded function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,149 +1,152 @@
-const cards = document.querySelectorAll('.memory-card');
 
-let hasFlippedCard = false; //When starting the game no card has been flipped yet.
-let firstCard, secondCard; // First clicked card and second clicked card in order to check for match
-let lockBoard = false;  
-const timeCounter = document.querySelector(".timer");
-const moveCounter = document.querySelector(".moves"); 
-let gameOn = false;
-let match=0; //Used for the match-counter
-let totalMoves=0;
-let totalTime="";
+document.addEventListener("DOMContentLoaded", function(event) { 
+  
+  const cards = document.querySelectorAll('.memory-card');
 
-const winMsg = document.getElementById("winMessageWindow");
+  let hasFlippedCard = false; //When starting the game no card has been flipped yet.
+  let firstCard, secondCard; // First clicked card and second clicked card in order to check for match
+  let lockBoard = false;  
+  const timeCounter = document.querySelector(".timer");
+  const moveCounter = document.querySelector(".moves"); 
+  let gameOn = false;
+  let match=0; //Used for the match-counter
+  let totalMoves=0;
+  let totalTime="";
+
+  const winMsg = document.getElementById("winMessageWindow");
 
 
 
-/* flipCard function with help from https://github.com/code-sketch/memory-game*/
-function flipCard() {
-  if (!gameOn) {
-    gameOn = true;
-    timer();
-}
-  if (lockBoard) return;
-  if (this === firstCard) return;
+  /* flipCard function with help from https://github.com/code-sketch/memory-game*/
+  function flipCard() {
+    if (!gameOn) {
+      gameOn = true;
+      timer();
+  }
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-  this.classList.add('flip');
+    this.classList.add('flip');
 
-  if (!hasFlippedCard) {
-    //If hasFlippedCard is false (first time the card is clicked) set the clicked card to firstCard
-    hasFlippedCard = true;
-    firstCard = this;
+    if (!hasFlippedCard) {
+      //If hasFlippedCard is false (first time the card is clicked) set the clicked card to firstCard
+      hasFlippedCard = true;
+      firstCard = this;
 
-    return;
+      return;
+    }
+
+    // second click
+    secondCard = this;
+
+    checkForMatch();
   }
 
-  // second click
-  secondCard = this;
+  // Check if the two cards clicked match (firstCard===secondCard)
+  function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
 
-  checkForMatch();
-}
-
-// Check if the two cards clicked match (firstCard===secondCard)
-function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-
-  isMatch ? disableCards() : unflipCards();
-}
-
-// When it´s a match, remove eventlisteners of the cards in order to prevent these cards to be clicked again
-function disableCards() {
-  firstCard.removeEventListener('click', flipCard);
-  secondCard.removeEventListener('click', flipCard);
-
-  resetBoard();
-  match++;  // When it´s a match, add to the match counter
-  
-  //When all mathes are found (8 pairs) the the timer stops and win message shows
-  if(match===8){
-    totalMoves= move;
-    clearInterval(time);
-    winMessage();
+    isMatch ? disableCards() : unflipCards();
   }
 
-  
-}
-
-/*Not a match
-  If not a match remove the flip class from the memory-card after 1000ms(1s) in order for these cards 
-  to be flipped back and in the game agin after that time.*/
-function unflipCards() {
-  lockBoard = true;
-
-  setTimeout(() => {
-    firstCard.classList.remove('flip');
-    secondCard.classList.remove('flip');
+  // When it´s a match, remove eventlisteners of the cards in order to prevent these cards to be clicked again
+  function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
 
     resetBoard();
-  }, 1000);
+    match++;  // When it´s a match, add to the match counter
+    
+    //When all mathes are found (8 pairs) the the timer stops and win message shows
+    if(match===8){
+      totalMoves= move;
+      clearInterval(time);
+      winMessage();
+    }
+
+    
+  }
+
+  /*Not a match
+    If not a match remove the flip class from the memory-card after 1000ms(1s) in order for these cards 
+    to be flipped back and in the game agin after that time.*/
+  function unflipCards() {
+    lockBoard = true;
+
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+
+      resetBoard();
+    }, 1000);
+    
+    // Add a move to the move-counter in the addMove function
+    addMove();
+    
+  }
+
+  function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+  }
+
+  (function shuffle() {
+    cards.forEach(card => {
+      let randomPos = Math.floor(Math.random() * 16);
+      card.style.order = randomPos;
+    });
+  })();
+
+
+  // Event listener listening for click events (on a memory card) which activates the flipCard function.
+  cards.forEach(card => card.addEventListener('click', flipCard));
+
+
+  //Move counter
+  let move = 0;
+  moveCounter.innerHtml = 0;
   
-  // Add a move to the move-counter in the addMove function
-  addMove();
+  function addMove() {
+      move++;
+      moveCounter.innerHTML = move;
+  }
+
+  //timer
+  let time;
+  let minutes = 00;
+  let seconds = 00;
+  let timeStart = false;
+  timeCounter.innerHTML = "Time: " + minutes + " : " + seconds;
   
-}
-
-function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
-}
-
-(function shuffle() {
-  cards.forEach(card => {
-    let randomPos = Math.floor(Math.random() * 16);
-    card.style.order = randomPos;
-  });
-})();
-
-
-// Event listener listening for click events (on a memory card) which activates the flipCard function.
-cards.forEach(card => card.addEventListener('click', flipCard));
-
-
-//Move counter
-let move = 0;
-moveCounter.innerHtml = 0;
- 
- function addMove() {
-     move++;
-     moveCounter.innerHTML = move;
- }
-
- //timer
- let time;
- let minutes = 00;
- let seconds = 00;
- let timeStart = false;
- timeCounter.innerHTML = "Time: " + minutes + " : " + seconds;
- 
- function timer() {
-     time = setInterval(function() {
-         seconds++;
-         if (seconds === 60) {
-             minutes++;
-             seconds = 0;
-         }
-         timeCounter.innerHTML = "Time " + minutes + " : " + seconds;
-     }, 1000);
- }
-
-
-  function stopTime() {
-  clearInterval(time);
+  function timer() {
+      time = setInterval(function() {
+          seconds++;
+          if (seconds === 60) {
+              minutes++;
+              seconds = 0;
+          }
+          timeCounter.innerHTML = "Time " + minutes + " : " + seconds;
+      }, 1000);
   }
 
 
-   // Win message pop up 
- function winMessage() {
-  winMsg.style.display = "block";
-  totalTime = timeCounter.innerHTML;
-  // showing moves and time on modal
-  document.getElementById("totalMoves").innerHTML = totalMoves;
-  document.getElementById("totalTime").innerHTML = totalTime;
-  //reset();
-}
+    function stopTime() {
+    clearInterval(time);
+    }
 
 
- 
+    // Win message pop up 
+  function winMessage() {
+    winMsg.style.display = "block";
+    totalTime = timeCounter.innerHTML;
+    // showing moves and time on modal
+    document.getElementById("totalMoves").innerHTML = totalMoves;
+    document.getElementById("totalTime").innerHTML = totalTime;
+    //reset();
+  }
+
+
+});
 
 
 
